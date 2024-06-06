3b8154cc10a80f19a4dac5cb345af8adda5a4621
allbrancereal <akko.sinn@gmail.com>
2024-06-06 06:26:47 -0700

    Update LStart.py

--- LStart/LStart.py
+++ LStart/LStart.py
@@ -14,24 +14,29 @@ def main(input_file=None, time_per_word=None, output_dir=None):
 
     # Define the path to the bin directory
     bin_dir = os.path.join(os.path.dirname(sys.executable), 'bin')
-
+    # Define the paths to the external scripts
+    scripts = ['LTextTagger.exe', 'LScriptGenerator.exe', 'LPremiereWavPrepare.exe']
+    script_paths = {script: os.path.join(bin_dir, script) for script in scripts}
+    
+    # Check if the external scripts exist
+    for script, path in script_paths.items():
+        if not os.path.exists(path):
+            print(f"Error: {script} does not exist at {path}")
+            return
+        
     # Run LTextTagger.py
     output_html_file = input_file.replace('.txt', '.html')
-    ltexttagger_path = os.path.join(bin_dir, 'LTextTagger.exe')
-    print(f"Running: {ltexttagger_path}")
-    subprocess.run([ltexttagger_path, '-file', input_file], check=True)
+    print(f"Running: {script_paths['LTextTagger.exe']}")
+    subprocess.run([script_paths['LTextTagger.exe'], '-file', input_file], check=True)
 
-    # Run LScriptGenerator.py
     output_json_file = os.path.join(output_dir, os.path.basename(output_html_file).replace('.html', '.json')) if output_dir else output_html_file.replace('.html', '.json')
-    lscriptgenerator_path = os.path.join(bin_dir, 'LScriptGenerator.exe')
-    print(f"Running: {lscriptgenerator_path}")
-    subprocess.run([lscriptgenerator_path, '-file', output_html_file, '-o', output_dir if output_dir else os.path.dirname(output_html_file)], check=True)
+    print(f"Running: {script_paths['LScriptGenerator.exe']}")
+    subprocess.run([script_paths['LScriptGenerator.exe'], '-file', output_html_file, '-o', output_dir if output_dir else os.path.dirname(output_html_file)], check=True)
 
     # Run LPremiereWavPrepare.py
-    lpremierewavprepare_path = os.path.join(bin_dir, 'LPremiereWavPrepare.exe')
-    print(f"Running: {lpremierewavprepare_path}")
-    subprocess.run([lpremierewavprepare_path, '-file', output_json_file, '-time_per_word', str(time_per_word)], check=True)
-
+    print(f"Running: {script_paths['LPremiereWavPrepare.exe']}")
+    subprocess.run([script_paths['LPremiereWavPrepare.exe'], '-file', output_json_file, '-time_per_word', str(time_per_word)], check=True)
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process a text file.')
     parser.add_argument('-file', help='the path to the input text file')
